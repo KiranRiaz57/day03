e675a8d88918a7011004851577d7663c348f5ba8
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-10 21:40:41 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,17 @@
+// Q7
+// addition
+// console.log(5+3);
+// substraction
+// console.log(10-2);
+// multiplication
+// console.log(4*2);
+// division
+// console.log(16/2);
+// Q8
+// console.log(5+3);
+// console.log(4+4);
+// console.log(6+2);
+// console.log(7+1);
+// Q9
+let favouriteNumber = 5;
+console.log("My favourite number is",favouriteNumber);
